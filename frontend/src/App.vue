74249d5bfdd8bf98a0197c60c37bb5e7adc38204
Marek Sagan <marek.sagan@protonmail.com>
2020-05-19 00:49:31 +0200

    Added the VueJS frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+  <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                <b-field>
+            <b-input placeholder="Search..."
+                type="search"
+                icon="magnify"
+                icon-clickable
+                @icon-click="searchIconClick">
+            </b-input>
+        </b-field>
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Index book</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <Search msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
